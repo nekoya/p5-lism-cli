8d4f13ec784874c214f41bb71948d41b8ddd90f2
Ryo Miyake <ryo.studiom@gmail.com>
2009-11-28 17:51:22 +0900

    initial commit

---
+++ lib/Lism/CLI.pm
@@ -0,0 +1,33 @@
+package Lism::CLI;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Lism::CLI -
+
+=head1 SYNOPSIS
+
+  use Lism::CLI;
+
+=head1 DESCRIPTION
+
+Lism::CLI is
+
+=head1 AUTHOR
+
+Ryo Miyake E<lt>ryo.studiom@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
